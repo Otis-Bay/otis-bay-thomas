fa37ce734fef9d5f5b1ee250d089b6e329ff1df4
Thomas Gertenbach <thomas.gertenbach99@gmail.com>
2023-10-10 16:16:21 +0200

    added experiment

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,113 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import React, { useRef, useEffect, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Environment, Lightformer, Stats } from "@react-three/drei";
+import "./styles.css";
+import { OtisBayLogo } from "./Letters";
+import { EffectComposer, N8AO, Noise } from "@react-three/postprocessing";
+import * as THREE from "three";
+import { BlendFunction } from "postprocessing";
+import { BallCollider, Physics, RigidBody } from '@react-three/rapier'
+import { Leva } from "leva";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
+const CameraControls = () => {
+  const { camera } = useThree()
+  useFrame((state) => {
+    state.camera.zoom = THREE.MathUtils.lerp(state.camera.zoom, 1, 0.15);
+    state.camera.lookAt(0, 1, 0);
+  });
 
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
+  //animate camera down when scrolling gsap scrolltrigger
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(camera.position, {
+      y: -30,
+      z: 50,
+      duration: 1,
+      scrollTrigger: {
+        trigger: ".container",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+  }, []);
+
+  return null;
+};
+
+
+function Pointer({ vec = new THREE.Vector3() }) {
   const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
+  useFrame(({ mouse, viewport }) => {
+    ref.current?.setNextKinematicTranslation(vec.set((mouse.x * viewport.width) / 3, (mouse.y * viewport.height) / 3, 0))
+  })
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
+    <RigidBody position={[0, 0, 0]} type="kinematicPosition" colliders={false} ref={ref}>
+      <BallCollider args={[1]} />
+    </RigidBody>
   )
 }
 
 export default function App() {
+
+
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <OrbitControls />
-    </Canvas>
-  )
+    <>
+      <Canvas
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: false }}
+        camera={{ position: [0, -1, 20], fov: 40 }}
+      >
+        <Leva hidden />
+        <Physics gravity={[0, 0, 0]}>
+          <OtisBayLogo
+            scale={1.1}
+          />
+          <Pointer />
+        </Physics>
+        {/* <Stats /> */}
+        <CameraControls />
+        <EffectComposer disableNormalPass multisampling={8}>
+          <Noise premultiply blendFunction={BlendFunction.ADD} />
+          <N8AO distanceFalloff={1} aoRadius={1} intensity={4} />
+        </EffectComposer>
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[10, 0, 10]} intensity={0.4} color="#fff" />
+        <Environment resolution={256}>
+          <group rotation={[-Math.PI / 3, 0, 1]}>
+            <Lightformer
+              form="circle"
+              intensity={4}
+              rotation-x={Math.PI / 2}
+              position={[0, 5, -9]}
+              scale={2}
+            />
+            <Lightformer
+              form="circle"
+              intensity={2}
+              rotation-y={Math.PI / 2}
+              position={[-5, 1, -1]}
+              scale={2}
+            />
+            <Lightformer
+              form="circle"
+              intensity={2}
+              rotation-y={Math.PI / 2}
+              position={[-5, -1, -1]}
+              scale={2}
+            />
+            <Lightformer
+              form="circle"
+              intensity={2}
+              rotation-y={-Math.PI / 2}
+              position={[10, 1, 0]}
+              scale={8}
+            />
+          </group>
+        </Environment>
+      </Canvas>
+    </>
+  );
 }
---
+++ src/Letters.jsx
@@ -0,0 +1,174 @@
+import React, { useRef, useState, useEffect } from "react";
+import { useCursor, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+import { useControls } from "leva";
+import gsap from "gsap";
+import { RigidBody } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+
+function Letter({ geometry, material, position, index, vec = new THREE.Vector3() }) {
+  const letterRef = useRef();
+  const [hovered, set] = useState()
+
+  const handleClick = () => {
+    // Create a GSAP animation to rotate the letter 360 degrees
+    gsap.to(letterRef.current.rotation, {
+      y: Math.PI * 2,
+      duration: 0.8, // Adjust the duration as needed
+      ease: "Power2.easeInOut",
+      onComplete: () => {
+        // Reset the rotation after the animation is complete
+        gsap.set(letterRef.current.rotation, { y: 0 });
+      },
+    });
+  };
+
+  const handleAnimation = () => {
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+      letterRef.current.position,
+      { y: -5, x: 5 },
+      { y: position[1], x: position[0], duration: 0.8, delay: 0.01 * index, ease: "power2.out" },
+      0.2 * index
+    );
+
+    tl.to(letterRef.current.rotation, {
+      y: Math.PI * 2,
+      duration: 1,
+      onComplete: () => {
+        gsap.set(letterRef.current.rotation, { y: 0 });
+      },
+    }, "-=1");
+
+    return tl;
+  };
+
+  useCursor(hovered, /*'pointer', 'auto', document.body*/)
+
+  // useEffect(() => {
+  //   handleAnimation();
+  // }, []);
+
+  const api = useRef()
+  useFrame((state, delta) => {
+    delta = Math.min(0.1, delta)
+    if (!api.current) return
+
+    api.current?.applyImpulse(vec.copy({
+      x: position[0] - api.current.translation().x,
+      y: position[1] - api.current.translation().y,
+      z: position[2] - api.current.translation().z
+    }).multiplyScalar(40))
+
+    // alos back to original rotation
+    api.current?.applyTorqueImpulse(vec.copy({
+      x: 0 - api.current.rotation().x,
+      y: 0 - api.current.rotation().y,
+      z: 0 - api.current.rotation().z
+    }).multiplyScalar(40))
+
+
+  })
+
+  return (
+    <RigidBody
+      type="Dynamic"
+      linearDamping={4} angularDamping={1} friction={0.1} position={position} ref={api} colliders='hull'>
+      <mesh
+        onPointerOver={() => set(true)}
+        onPointerOut={() => set(false)}
+        castShadow
+        receiveShadow
+        geometry={geometry}
+        material={material}
+        ref={letterRef}
+        onClick={handleClick}
+      />
+    </RigidBody>
+  );
+}
+
+export function OtisBayLogo(props) {
+  const { nodes } = useGLTF("http://localhost:5173/otis-bay.glb");
+  const { color1, color2 } = useControls({
+    color1: "#41F4D5",
+    color2: "#9069F8",
+  });
+
+  const materials = {
+    materialForOTIS: new THREE.MeshStandardMaterial({
+      color: color1,
+      metalness: 0,
+      roughness: 0.2,
+    }),
+
+    materialForBAY: new THREE.MeshPhysicalMaterial({
+      color: color2,
+      metalness: 0,
+      roughness: 0.3,
+    }),
+  };
+
+  const letters = [
+    {
+      name: "O",
+      geometry: nodes.O.geometry,
+      material: materials.materialForOTIS,
+      position: [-7.01, 3.769, -1.54]
+    },
+
+    {
+      name: "T",
+      geometry: nodes.T.geometry,
+      material: materials.materialForOTIS,
+      position: [-1.122, 5.764, -1.54]
+    },
+    {
+      name: "I",
+      geometry: nodes.I.geometry,
+      material: materials.materialForOTIS,
+      position: [3.552, 3.42, -1.54]
+    },
+    {
+      name: "S",
+      geometry: nodes.S.geometry,
+      material: materials.materialForOTIS,
+      position: [7.747, 5.97, -1.54]
+    },
+    {
+      name: "B",
+      geometry: nodes.B.geometry,
+      material: materials.materialForBAY,
+      position: [-6.812, -3.059, -1.54]
+    },
+    {
+      name: "A",
+      geometry: nodes.A.geometry,
+      material: materials.materialForBAY,
+      position: [-0.728, -2.75, -1.54]
+    },
+    {
+      name: "Y",
+      geometry: nodes.Y.geometry,
+      material: materials.materialForBAY,
+      position: [4.372, -3.529, -1.54]
+    }
+  ];
+
+  return (
+    <group {...props} dispose={null}>
+      {letters.map((letter, index) => (
+        <Letter
+          key={index}
+          index={index}
+          position={letter.position}
+          geometry={letter.geometry}
+          material={letter.material}
+        />
+      ))}
+    </group>
+  );
+}
+
+useGLTF.preload("http://localhost:5173/otis-bay.glb");
